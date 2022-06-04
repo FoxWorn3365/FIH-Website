659411321e19e38af6f331e6602bb6f0fcff9ad4
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-06-04 12:56:28 +0200

    File della pagina index principale

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+require_once("assets/header.php");
+?>
+   <br><br>
+   <div id='presentazione' style='padding: 18px'>
+    <a id='chi-siamo'>
+    <div id='azienda' class='w3-container fox-azienda-promo-div'>
+     <h1 id='chisiamo' class='fox-aln w3-animate-left'>Chi Siamo</h1>
+     <span id='chisiamoContext' class='fox-aln w3-animate-bottom'>
+      La FoxInvest Holding nasce nel Febbraio del 2021 a Karabash, fondata da FoxWorn3365.<br>
+      Da Marzo 2022, con l'assunzione di Ghostmike_ (ex Frigorifero005) la FoxInvest Holding<br>
+      ha iniziato a produrre oggetti per la ComputerCraft in quantità destinate alla vendita al dettaglio.<br>
+      Durante Maggio 2021 la FoxInvest Holding si è espansa anche a New Radeon acquistando da<br>
+      _Musy_, l'edificio "Rosso" vicino a New Radeon Moria, per produrre UU-Matter.<br>
+      Sempre durante lo stesso mese è stata donata da Fra146 la sua azienda a Neihemiah, protetta<br>
+      da ENDOR e pensata per produrre petrolio e UU-Matter.<br>
+      A Giugno 2021 è stata edificata la Fiumara, a New Radeon ZI, per produrre altra UU-Matter<br>
+      in un edificio moderno e con una rete elettrica innovativa per le altre presenti nei terreni della FIH.<br>
+      La fabbrica di Neihemiah venne ristrutturata verso Luglio 2021 in qualche giorno,<br>
+      per un maggior risparmio energetico e per incrementare la produzione di UU-Matter<br>
+      Da Marzo 2022 la FoxInvest è operativa nelle 2 città principali.<br><br>
+     </span>
+    </div>
+    </a>
+    <br>
+    <a id='numeri'>
+    <div id='inostrinumeri' class='w3-container fox-azienda-promo-div-left'>
+     <h1 id='inostrinum_title' class='fox-aln w3-animate-left'>I Nostri Numeri</h1>
+     <span id='inostrinumeriContext' class='fox-aln w3-animate-bottom'>
+      <b><span id='stabilimenti' class='fox-counter'>1</span></b> <span class='fox-counter-text'>Stabilimenti nella Nazione</span><br>
+      <b><span id='fatturato' class='fox-counter'>0</span></b><span class='fox-counter'>k IC</span> <span class='fox-counter-text'>Fatturato Mensile</span><br>
+      <b><span id='progetti_real' class='fox-counter'>0</span></b> <span class='fox-counter-text'>Progetti Realizzati</span><br>
+      <b><span id='mw' class='fox-counter'>0</span></b> <span class='fox-counter'>MW</span> <span class='fox-counter-text'>Di energia elettrica consumati da Noi<br>
+     </span>
+    </div>
+    </a>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+   </div>
+   <!--
+   <div id='prodotti' class='fox-prodotti'>
+    <a id='prodotti'>
+    <div id='prod_pres' class='w3-container fox-azienda-prodotti-main'>
+     <h1 class='fox-aln'>I nostri Prodotti</h1>
+    </div>
+    </a>
+   </div>
+   -->
+   <center><span>&copy; 2021 - 2022 FoxInvest Holding by FoxWorn3365</span></center>
+  </div>
+ </body>
+ <footer class='w3-container w3-bottom w3-text-black'>
+  <span><br><br><br><br><br><br></span>
+  <script src='assets/js/counter_number.js'></script>
+  <script>
+  animateValue('stabilimenti', 1, 6, 5000);
+  animateValue('fatturato', 0, '1500', 3500);
+  animateValue('progetti_real', 0, 12, 4500);
+  animateValue('mw', 0, 10, 4550);
+  </script>
+ </footer>
+</html>
