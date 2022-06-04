8fda21cfe361f61a463a1d5dab86305a8feed8fa
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-06-04 12:54:49 +0200

    Prima versione ALPHA
    Ancora incompleta, ci sto lavorando su

---
+++ assets/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>FoxInvest Holding</title>
+  <meta name="title" content="FoxInvest Holding">
+  <meta name="description" content="Il sito ufficiale della FoxInvest Holding, una delle aziende più influenti su RGBCraft">
+  <meta name="keywords" content="RGBCraft, Fox, FoxInvest, FoxWorn3365, FoxInvest Holding">
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="revisit-after" content="2 days">
+  <meta name="author" content="Federico Cosma">
+  <link rel="icon" type="image/png" href="assets/img/logo.png">
+  <link rel="stylesheet" href="assets/css/GothamBlack.css">
+  <link rel="stylesheet" href="assets/css/GothamMedium.css">
+  <link rel="stylesheet" href="assets/css/w3.css">
+  <link rel="stylesheet" href="assets/css/home.css">
+ </head>
+ <body id='body' class='fox-main'>
+  <div class="w3-bar w3-top" style='font-size: 20px'>
+   <a href="/" class="w3-bar-item" style='text-decoration: none'><span style='color: orange; font-size: 30px'><b>FIH</b></a>
+   <a href="#chi-siamo" class="w3-bar-item w3-button" style='padding: 15px'>Chi Siamo</a>
+   <a href="#numeri" class="w3-bar-item w3-button" style='padding: 15px'>I nostri Numeri</a>
+   <a href="#prodotti" class="w3-bar-item w3-button" style='padding: 15px'>Prodotti</a>
+   <a href="/us/" class="w3-bar-item w3-button w3-right" style='padding: 15px'>Cosa Facciamo</a>
+  </div> 
+  <div class='w3-container mainVideoDiv'>
+   <video autoplay muted loop class='mainVideo'>
+    <source src="assets/video/mediaset.mp4" type="video/mp4">
+   </video>
+  </div>
+  <div id='mainText' class='w3-container w3-display-middle w3-xxl'>
+   <h1 style='font-size: 75px' class='w3-animate-left'>Guardiamo il Futuro</h1>
+  </div>
+  <br><br><br><br><br><br>
+  <div id='paragrafo' class='w3-container fox-hide-all-par'>
+
